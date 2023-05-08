cac4c1f218afc8659d0d4a206076c3dd4eb1a2ef
MatheusAlatxeve <114530956+MatheusAlatxeve@users.noreply.github.com>
2023-05-08 09:01:47 -0300

    aula9

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sub_of_values(number1, number2):
+    result = number1 - number2
+    return result
+
+def sum_of_values(number1, number2):
+    result = number1 + number2
+    return result
+
+def div_of_values(number1, number2):
+    result = (number1 / number2)
+    return result
+
+def mult_of_values(number1, number2):
+    result = number1 * number2
+    return result
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sub():
+    #given two numbers 
+    number1 = 5
+    number2 = 2
+ 
+    #when we subtract the two numbers
+    output = methods.sub_of_values(number1, number2)
+
+    #then the subtraction must be
+    assert output == 3
+
+def test_sum():
+    #given two numbers 
+    number1 = 8
+    number2 = 2
+ 
+    #when we add the two numbers
+    output = methods.sum_of_values(number1, number2)
+
+    #then the sum must be
+    assert output == 10
+
+def test_div():
+    #given two numbers 
+    number1 = 4
+    number2 = 2
+ 
+    #when we divide the two numbers
+    output = methods.div_of_values(number1, number2)
+
+    #then the division must be
+    assert output == 2
+
+def test_mult():
+    #given two numbers 
+    number1 = 5
+    number2 = 3
+ 
+    #when we multiply the two numbers
+    output = methods.mult_of_values(number1, number2)
+
+    #then the multiplication must be
+    assert output == 15
